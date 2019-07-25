f2b16e2eab2baf925699146abff3c6ab1edb7068
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-07-26 01:51:11 +0800

    added repo command

---
+++ commands/repo.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'repo',
+  description: 'Links the repository for this bot',
+  execute(message) {
+    message.channel.send('https://github.com/rayhanw/discord-bot');
+  },
+};
