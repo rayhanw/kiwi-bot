e9767e59d1c567b4cede5cdda674a97e421784ec
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-07-27 00:20:51 +0800

    added ass

---
+++ commands/ass.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'ass',
+  description: 'ass compliment',
+  execute(message) {
+    if (!message.mentions.users.size) {
+      return message.channel.send(`Nice ass: @${message.author.username}`);
+    }
+
+    const assCompliment = message.mentions.users.map(user => `Nice ass @${user.username}`);
+
+    message.channel.send(assCompliment);
+  },
+};
