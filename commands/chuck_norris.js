3ec1f550980034efc835e12ba8ebc2885c32771d
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-07-26 13:40:15 +0800

    added chuck norris fact

---
+++ commands/chuck_norris.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+  name: 'chuck-norris',
+  description: 'Get a random Chuck Norris fact',
+  execute(message) {
+    const url = 'https://api.chucknorris.io/jokes/random';
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        message.channel.send(`Chuck Norris fact of the day is: **${data.value}**\n${data.icon_url}`);
+      });
+  },
+};
