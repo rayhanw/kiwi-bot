5a4ccde869776a6390322bdc8c9f33e1b4f2a2d3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-09-16 14:03:58 +0800

    added discord testing

---
+++ tests/discord.test.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const discordTest = require("./discord");
+
+const commandFiles = fs
+	.readdirSync("./commands")
+	.filter(file => file.endsWith(".js"));
+
+test("there should be 16 files", () => {
+	// Change the argument of toBe the amount of files within the /commands folder
+	expect(discordTest(commandFiles)).toBe(16);
+});
