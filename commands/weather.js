939e1dc99005deef51f38c33cdadb7ae34ef21f9
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-10-10 11:18:28 +0800

    weather command now accepts double worded (or more) city names

--- commands/weather.js
+++ commands/weather.js
@@ -5,9 +5,9 @@ module.exports = {
 	description: "Gets the weather of a city",
 	args: true,
 	execute(message, args) {
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${
-			args[0]
-		}&appid=${process.env.OWM_API}&units=metric`;
+		const city = args.join(" ");
+		console.log(city);
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OWM_API}&units=metric`;
 		fetch(url)
 			.then(response => response.json())
 			.then(data => {
