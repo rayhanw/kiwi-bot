424a43726bc2141cf868f83699f933248f05a8d7
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-08 22:24:46 +0700

    mention owner

--- commands/owner.js
+++ commands/owner.js
@@ -1,7 +1,7 @@
 module.exports = {
-	name: "owner",
-	description: "Gets the owner's name",
-	execute(message) {
-		message.channel.send("Homosaurus is the owner of the channel.");
-	}
+  name: "owner",
+  description: "Gets the owner's name",
+  execute(message) {
+    message.channel.send("<@106345742565654528> is the owner of the channel.");
+  },
 };
