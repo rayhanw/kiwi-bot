7b16fef863619850f37ef07e9ad76e6ff3ea685d
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-08 22:11:10 +0700

    mention

--- commands/ass.js
+++ commands/ass.js
@@ -1,12 +1,14 @@
 module.exports = {
-  name: 'ass',
-  description: 'ass compliment',
+  name: "ass",
+  description: "ass compliment",
   execute(message) {
     if (!message.mentions.users.size) {
       return message.channel.send(`Nice ass: @${message.author.username}`);
     }
 
-    const assCompliment = message.mentions.users.map(user => `Nice ass ${user.username}`);
+    const assCompliment = message.mentions.users.map(
+      (user) => `Nice ass <@${user.username}>`
+    );
 
     message.channel.send(assCompliment);
   },
