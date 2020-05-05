39a93a0693f5523d38452f6966e5f9b1eb3b5e57
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-05-05 17:09:31 +0700

    update ip

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "minecraft",
   description: "Shows the minecraft server",
   execute(message) {
-    message.channel.send("IP: `54.254.194.32:25565`");
-    message.channel.send("If you want vanilla, ask admin for an invite!");
+    message.channel.send("IP: `51.79.152.235:25577`");
+    message.channel.send("If you want to play vanilla Minecraft, ask admin for an invite!");
   }
 };
