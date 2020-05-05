1883b8d99f29ffd5976f566213ea7eccdbc2bc30
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-05-05 14:15:00 +0700

    change minecraft IP

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "minecraft",
   description: "Shows the minecraft server",
   execute(message) {
-    message.channel.send("IP: `capzailina.aternos.me`");
+    message.channel.send("IP: `54.254.194.32:25565`");
+    message.channel.send("If you want vanilla, ask admin for an invite!");
   }
 };
