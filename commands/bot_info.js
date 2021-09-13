ae1fe46fd70c41a6d42ac1ea57cace229d987802
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-09-14 00:14:24 +0700

    update bot-info

--- commands/bot_info.js
+++ commands/bot_info.js
@@ -4,25 +4,31 @@ module.exports = {
   name: "bot-info",
   description: "Shows the bot information",
   execute(message) {
-    const exampleEmbed = new Discord.RichEmbed()
-      .setColor("#fff")
-      .setTitle("Kiwi bot")
-      .setURL(
-        "https://discordapp.com/oauth2/authorize?client_id=603952919536074777&scope=bot"
-      )
-      .setAuthor(
-        "Rayhan A",
-        "https://rayhanw.com/images/profile.jpg",
-        "https://rayhanw.com"
-      )
-      .setDescription("A simple 'ole bot that does fuck all")
-      .setThumbnail("https://i.imgur.com/BX1HZTr.jpg")
-      .addField("hmm", "I can do fuck all")
-      .addBlankField()
-      .setImage("https://i.imgur.com/BX1HZTr.jpg")
-      .setTimestamp()
-      .setFooter("I do fuckall", "https://i.imgur.com/BX1HZTr.jpg");
+    const embed = new Discord.MessageEmbed({
+      color: "#fff",
+      title: "Kiwi bot",
+      url: "https://discordapp.com/oauth2/authorize?client_id=603952919536074777&scope=bot",
+      author: {
+        name: "Domosaurus",
+        url: "https://domosaurus.me",
+        iconURL: "https://www.domosaurus.me/img/canopus_lux.4940a7a8.png"
+      },
+      description: "A simple 'ole bot that does fuck all",
+      fields: [
+        {
+          name: "Hmm",
+          value: "I can do fuck all"
+        }
+      ],
+      image: {
+        url: "https://i.imgur.com/BX1HZTr.jpg"
+      },
+      footer: {
+        text: "I do fuckall",
+        iconURL: "https://i.imgur.com/BX1HZTr.jpg"
+      }
+    });
 
-    message.channel.send(exampleEmbed);
+    message.channel.send({ embeds: [embed] });
   }
 };
