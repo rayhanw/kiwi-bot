9cdf039c28aacd9073f841d913eaecd0e42cfa30
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-11-29 15:53:10 +0800

    remove play

--- commands/play.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "play",
-  description: "Plays a song from Youtube",
-  args: true,
-  async execute(message, args) {
-    console.log({ channel: message.channel, args });
-  }
-};
