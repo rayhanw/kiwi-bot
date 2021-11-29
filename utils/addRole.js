8ca45295d067ec40f7fc4784f0dcdb570730946b
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-11-29 19:55:12 +0800

    use different way to fetch user

--- utils/addRole.js
+++ utils/addRole.js
@@ -1,16 +1,16 @@
 const { ROLES, EMOJIS } = require("./constants");
 
-const addRole = (reaction, user) => {
-  console.log("Executing adding role");
+const addRole = async (reaction, user) => {
+  const discordUser = await reaction.message.guild.members.cache.get(user.id);
 
   if (reaction.emoji.id === EMOJIS.genshin) {
-    reaction.message.member.roles.add(ROLES.genshin);
+    discordUser.roles.add(ROLES.genshin);
     console.log(`${user.username} was given the Genshin role`);
   } else if (reaction.emoji.id === EMOJIS.ffxiv) {
-    reaction.message.member.roles.add(ROLES.ffxiv);
+    discordUser.roles.add(ROLES.ffxiv);
     console.log(`${user.username} was given the FFXIV role`);
   } else if (reaction.emoji.id === EMOJIS.phasmophobia) {
-    reaction.message.member.roles.add(ROLES.phasmophobia);
+    discordUser.roles.add(ROLES.phasmophobia);
     console.log(`${user.username} was given the Phasmophobia role`);
   }
 };
--- utils/removeRole.js
+++ utils/removeRole.js
@@ -1,16 +1,16 @@
 const { ROLES, EMOJIS } = require("./constants");
 
 const removeRole = async (reaction, user) => {
-  console.log("Executing removing role");
+  const discordUser = await reaction.message.guild.members.cache.get(user.id);
 
   if (reaction.emoji.id === EMOJIS.genshin) {
-    reaction.message.member.roles.remove(ROLES.genshin);
+    discordUser.roles.remove(ROLES.genshin);
     console.log(`Genshin role was removed from ${user.username}`);
   } else if (reaction.emoji.id === EMOJIS.ffxiv) {
-    reaction.message.member.roles.remove(ROLES.ffxiv);
+    discordUser.roles.remove(ROLES.ffxiv);
     console.log(`FFXIV role was removed from ${user.username}`);
   } else if (reaction.emoji.id === EMOJIS.phasmophobia) {
-    reaction.message.member.roles.remove(ROLES.phasmophobia);
+    discordUser.roles.remove(ROLES.phasmophobia);
     console.log(`Phasmophobia role was removed from ${user.username}`);
   }
 };
